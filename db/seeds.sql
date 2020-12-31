bee6347ee019cb33f376a7b6d69f575c2e0f81cf
Jessica Whitman <oprah135@gmail.com>
2020-12-31 12:21:14 -0800

    Database setup. Seed data created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db
+INSERT INTO burgers (burger_name, devoured) VALUE ("Veggie Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUE ("Double Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUE ("BBQ Burger", true);
